f5f22a26eb79f1a1ab850e174952642370033266
Adam Chomicki <fadwick@live.com>
2017-03-15 09:12:12 +0100

    Add game logic

--- cppPong/main.cpp
+++ cppPong/main.cpp
@@ -1,9 +1,52 @@
 #include <SDL.h>
+#include <iostream>
+
+class Player 
+{
+public:
+	int xValPlayer;
+	int yValPlayer;
+
+	Player()
+	{
+		xValPlayer = 0;
+		yValPlayer = 0;
+	}
+};
+
+class Opponent
+{
+public:
+	int xValOpponent;
+	int yValOpponent;
+
+	Opponent()
+	{
+		xValOpponent = 0;
+		yValOpponent = 0;
+	}
+};
+
+class Pong
+{
+public:
+	int xValPong;
+	int yValPong;
+
+	Pong()
+	{
+		xValPong = 0;
+		yValPong = 0;
+	}
+};
 
 SDL_Window* createWindow(const int& xWinPos, const int& yWinPos, const int& xRes, const int& yRes,
 						SDL_Window* window);
-SDL_Renderer* createRenderer(SDL_Window *window, SDL_Renderer *renderer);
-void rendering(SDL_Renderer *renderer);
+
+SDL_Renderer* createRenderer(SDL_Window *window, SDL_Renderer *);
+void gameLogic(const int &xRes, const int &yRes, SDL_Renderer *renderer, Player *player, Opponent *opponent, Pong *pong);
+void rendering(SDL_Renderer *renderer, const int &xRes, Player *player, Opponent *opponent, Pong *pong);
+void clean(Player *player, Opponent *opponent, Pong *pong);
 
 int main(int argc, char **argv)
 {
@@ -17,6 +60,10 @@ int main(int argc, char **argv)
 	
 	SDL_Renderer* renderer = nullptr;
 	renderer = createRenderer(window, renderer);
+
+	Player *player = new Player;
+	Opponent *opponent = new Opponent;
+	Pong *pong = new Pong;
 
 	auto done = false;
 	SDL_Event event;
@@ -35,9 +82,12 @@ int main(int argc, char **argv)
 				break;
 			}
 		}
-		rendering(renderer);
+
+		gameLogic(xRes, yRes, renderer, player, opponent, pong);
+		rendering(renderer, xRes, player, opponent, pong);
 	}
 
+	clean(player, opponent, pong);
 	SDL_Quit();
 	return 0;
 }
@@ -56,12 +106,79 @@ SDL_Renderer* createRenderer(SDL_Window *window, SDL_Renderer *renderer)
 	return renderer = SDL_CreateRenderer(window, -1, 0); // window, index, flags
 }
 
-void rendering(SDL_Renderer *renderer)
+void gameLogic(const int &xRes, const int &yRes, SDL_Renderer *renderer, Player *player, Opponent *opponent, Pong *pong)
 {
+
+	pong->xValPong = xRes / 2;
+	pong->yValPong = yRes / 2;
+	auto xPongV = 3;
+	auto yPongV = 3;
+
+	pong->xValPong += xPongV;	// movement
+	pong->yValPong += yPongV;
+
+	if (pong->yValPong < 0 && yPongV < 0) { yPongV *= -1; }		// is ball flying
+	
+	if (pong->yValPong > yRes && yPongV > 0) { yPongV *= -1; }	// vertical movement
+	
+	if (pong->xValPong < 0 && xPongV < 0)
+	{
+		xPongV *= -1;
+		if (pong->yValPong < player->yValPlayer || pong->yValPong > player->yValPlayer + 50)
+		{
+			// ++aiScore;
+			pong->xValPong = xRes / 2;
+			pong->yValPong = yRes / 2;
+		}
+	}
+
+	if (pong->xValPong > xRes && xPongV > 0)
+	{
+		xPongV *= -1;
+		if (pong->yValPong < opponent->yValOpponent || pong->yValPong > opponent->yValOpponent + 50)
+		{
+			// ++playerScore
+			pong->xValPong = xRes / 2;
+			pong->yValPong = yRes / 2;
+		}
+	}
+}
+
+void rendering(SDL_Renderer *renderer, const int &xRes, Player *player, Opponent *opponent, Pong *pong)
+{
+	
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
 	SDL_RenderClear(renderer);
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 
+	SDL_Rect rect;
+	rect.x = 15;
+	rect.y = player->yValPlayer;
+	rect.w = 5;
+	rect.h = 50;
+	SDL_RenderFillRect(renderer, &rect);
+
+	rect.x = xRes - 15;
+	rect.y = opponent->yValOpponent;
+	rect.w = 5;
+	rect.h = 50;
+	SDL_RenderFillRect(renderer, &rect);
+
+	rect.x = pong->xValPong - 5;
+	rect.y = pong->yValPong - 5;
+	rect.w = 5;
+	rect.h = 5;
+	SDL_RenderFillRect(renderer, &rect);
+
 	SDL_RenderPresent(renderer);
 
+	// SDL_RenderPresent(renderer);
+
+}
+
+void clean(Player *player, Opponent *opponent, Pong *pong)
+{
+	delete player;
+	delete opponent;
+	delete pong;
 }
